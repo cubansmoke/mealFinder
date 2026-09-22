882f87bc4923eef1b96d9832b110647738c29c65
cubansmoke <149361048+cubansmoke@users.noreply.github.com>
2026-09-22 18:36:53 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/mealFinder/', // <--- Fixed: Added leading and trailing slashes
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
